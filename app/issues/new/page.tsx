2c0b074444d46939e6df7ad1553e5ef23e888b48
Jeremy <jeremy.togonon.dev@gmail.com>
2024-10-05 17:53:21 +0800

    Client side handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,35 +1,61 @@
 'use client';
-import { Box, Button, TextArea, TextField } from '@radix-ui/themes';
+import { Button, Callout, TextArea, TextField, Text } from '@radix-ui/themes';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
-interface IssueForm {
-  title: string;
-  description: string;
-}
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema, IssueForm } from '@/app/validationSchema';
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
-
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const [error, setError] = useState('');
   const onSubmit = async (data: IssueForm) => {
-    await axios.post('/api/issues', data);
-    router.push('/issues');
-    console.log(data.title);
+    try {
+      await axios.post('/api/issues', data);
+      router.push('/issues');
+    } catch (error) {
+      setError('An unexpected error occurred.');
+    }
   };
   return (
-    <form className='max-w-xl space-y-3' onSubmit={handleSubmit(onSubmit)}>
-      <TextField.Root placeholder='Title' {...register('title')} />
-      {/* This just an example of Controller, TextArea supports register as field */}
-      <Controller
-        name='description'
-        control={control}
-        render={({ field }) => (
-          <TextArea placeholder='Description' {...field} />
+    <div className='max-w-xl'>
+      {error && (
+        <Callout.Root color='red' className='mb-5'>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className='space-y-3' onSubmit={handleSubmit(onSubmit)}>
+        <TextField.Root placeholder='Title' {...register('title')} />
+        {errors.title && (
+          <Text color='red' as='p'>
+            {errors.title.message}
+          </Text>
         )}
-      />
-      <Button>Submit New Issue</Button>
-    </form>
+        {/* This just an example of Controller, TextArea supports register as field */}
+        <Controller
+          name='description'
+          control={control}
+          render={({ field }) => (
+            <TextArea placeholder='Description' {...field} />
+          )}
+        />
+        {errors.description && (
+          <Text color='red' as='p'>
+            {errors.description.message}
+          </Text>
+        )}
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
