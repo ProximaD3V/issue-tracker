7dbd426bd11287f7942edf8f5a8cf8b4d6a2178b
Jeremy <jeremy.togonon.dev@gmail.com>
2024-10-05 16:45:55 +0800

    Handle Form Submit

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,35 @@
+'use client';
 import { Box, Button, TextArea, TextField } from '@radix-ui/themes';
-import React from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { useForm, Controller } from 'react-hook-form';
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
+  const onSubmit = async (data: IssueForm) => {
+    await axios.post('/api/issues', data);
+    router.push('/issues');
+    console.log(data.title);
+  };
   return (
-    <Box className='max-w-xl space-y-3'>
-      <TextField.Root placeholder='Title' />
-      <TextArea placeholder='Description' />
+    <form className='max-w-xl space-y-3' onSubmit={handleSubmit(onSubmit)}>
+      <TextField.Root placeholder='Title' {...register('title')} />
+      {/* This just an example of Controller, TextArea supports register as field */}
+      <Controller
+        name='description'
+        control={control}
+        render={({ field }) => (
+          <TextArea placeholder='Description' {...field} />
+        )}
+      />
       <Button>Submit New Issue</Button>
-    </Box>
+    </form>
   );
 };
 
