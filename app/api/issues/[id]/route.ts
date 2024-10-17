8291bf62cb461046790ca4af852c61c3a45d38b8
Jeremy <jeremy.togonon.dev@gmail.com>
2024-10-17 20:47:42 +0800

    API for deleting issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,6 +1,7 @@
 import prisma from '@/prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
 import { issueSchema } from '../schema';
+import { error } from 'console';
 
 export async function PATCH(
   request: NextRequest,
@@ -29,4 +30,20 @@ export async function PATCH(
   });
 
   return NextResponse.json(updatedIssue);
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: 'Invalid Issue' }, { status: 404 });
+
+  await prisma.issue.delete({ where: { id: issue.id } });
+
+  return NextResponse.json({});
 }
